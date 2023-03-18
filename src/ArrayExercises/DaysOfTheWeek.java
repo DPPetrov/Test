d88318619918cb48b8221b15e5ff36dd1f948e74
Dragomir Petrov <dragomirpetrov7@gmail.com>
2023-03-18 17:54:33 +0200

    Added days of the week

---
+++ src/ArrayExercises/DaysOfTheWeek.java
@@ -0,0 +1,12 @@
+package ArrayExercises;
+
+import java.util.Scanner;
+
+public class DaysOfTheWeek {
+
+    Scanner sc = new Scanner(System.in);
+
+    String[] daysArr = {"Monday", "Tuesday", "Wednesday", "Thursday","Friday","Saturday", "Sunday"};
+
+
+}
